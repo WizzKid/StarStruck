b08a251aea2ca3b458b7c97cf73c6da5de52376e
Sbaero <skbaeringer@yahoo.com>
2017-12-30 18:54:47 -0500

    Posting for portfolio
    Controls camera position relative to player position on screen.

---
+++ CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	private GameObject player;
+    //private float Z_Offset;
+    public float Z_Offset_Start;
+    public float Z_Offset_Scaler = 1.0f;
+
+    public float followDist = 2.0f;
+    public float cameraLerp = 1.0f;
+    private Vector3 newPos;
+
+	// Use this for initialization
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		Z_Offset_Start = player.transform.position.z - transform.position.z;
+        //Z_Offset = Z_Offset_Start;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		newPos = new Vector3(player.transform.position.x/followDist, player.transform.position.y/followDist, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPos, cameraLerp);
+    }
+}
